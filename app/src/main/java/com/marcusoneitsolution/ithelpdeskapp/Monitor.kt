ff5e5fdf1a3d300f5a5cfc7cf9e8c73fec5b431a
Mark Ronnel Camilon <markcamilon@live.com>
2018-12-15 14:31:10 +0800

    Change to markcamilon@live.com added monitor activity Monitor list

--- app/src/main/java/com/marcusoneitsolution/ithelpdeskapp/Monitor.kt
+++ app/src/main/java/com/marcusoneitsolution/ithelpdeskapp/Monitor.kt
@@ -3,6 +3,8 @@ package com.marcusoneitsolution.ithelpdeskapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+// Displays all Monitor in the inventory
+
 class Monitor : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
